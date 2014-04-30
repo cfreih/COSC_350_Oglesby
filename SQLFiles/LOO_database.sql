7c305898feb7293aecfd7f665020d5739f0fff6d
cva9222 <carcecascante10@ole.augie.edu>
2014-04-30 18:20:00 -0500

    SQL Source and demo added to SQL file folder
    Need to install SQL for it to work

--- SQLFiles/LOO_database.sql
+++ SQLFiles/LOO_database.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS LOO_database;
 CREATE DATABASE IF NOT EXISTS LOO_database;
 
 USE LOO_Database;
@@ -6,93 +7,115 @@ DROP TABLE IF EXISTS painters;
 DROP TABLE IF EXISTS auction_paintings;
 DROP TABLE IF EXISTS inventory_paintings;
 
-CREATE TABLE painters (
-	painterID INT NOT NULL AUTO_INCREMENT,
-	firstName varchar (20) NOT NULL,
-	lastName varchar (20) NOT NULL,
-	fashionability INT NOT NULL,
-	PRIMARY KEY(painterID)
+CREATE TABLE artists (
+	artistID INT NOT NULL AUTO_INCREMENT,
+	firstName varchar (20),
+	lastName varchar (20),
+	fashionability DECIMAL,
+	PRIMARY KEY(artistID)
 ) ENGINE=InnoDB
 ;
 
 CREATE TABLE auction_paintings (
-	painterID INT NOT NULL,
-	title varchar (40) NOT NULL,
+	artistID INT NOT NULL,
+	title varchar (40),
 	dateOfWork INT,
 	dateOfAuction DATE,
-	salePrice INT,
-	height INT,
-	widht INT,
+	salePrice DECIMAL,
+	height DECIMAL,
+	width DECIMAL,
 	medium varchar (25),
 	subject varchar (25),
 	INDEX(medium),
 	INDEX(subject),
-	FOREIGN KEY(painterID) REFERENCES painters(painterID)
+	FOREIGN KEY(artistID) REFERENCES painters(artistID)
 ) ENGINE=InnoDB
 ;
 
 CREATE TABLE inventory_paintings (
-	painterID INT NOT NULL,
-	title varchar (40) NOT NULL,
+	artistID INT NOT NULL,
+	title varchar (40),
 	dateOfWork INT,
-	clasification varchar(30) NOT NULL,
-	height INT,
-	widht INT,
+	clasification varchar(30),
+	height DECIMAL,
+	width DECIMAL,
 	medium varchar (25),
 	subject varchar (25),
 	dateOfPurchase DATE,
-	nameOfSeller varchar(30) NOT NULL,
-	addressOfSeller varchar(40) NOT NULL,
-	maximunPurchasePrice INT,
-	actualPurchasePrice INT,
-	targetSellingPrice INT,
-	sold CHAR(1),
+	nameOfSeller varchar(30) ,
+	addressOfSeller varchar(40) ,
+	maximunPurchasePrice DECIMAL,
+	actualPurchasePrice DECIMAL,
+	targetSellingPrice DECIMAL,
+	sold BOOLEAN DEFAULT FALSE,
 	dateOfSale DATE,
 	nameOfBuyer varchar(30),
 	addressOfBuyer varchar(40),
-	actualSellingPrice INT,
-	INDEX(clasification),
-	FOREIGN KEY(painterID) REFERENCES painters(painterID)
+	actualSellingPrice DECIMAL,
+	flagBoughtReport BOOLEAN DEFAULT FALSE,
+	flagSoldReport BOOLEAN DEFAULT FALSE,
+	INDEX(dateOfPurchase),
+	INDEX(dateOfSale),
+	FOREIGN KEY(artistID) REFERENCES painters(artistID)
 ) ENGINE=InnoDB
 ;
-
+/* 
 insert into painters (firstName, lastName, fashionability) values ('Sam','Bock','10');
 insert into painters (firstName, lastName, fashionability) values ('Claudio','Arce','11');
 insert into painters (firstName, lastName, fashionability) values ('Tim','Burwitz','20');
 insert into painters (firstName, lastName, fashionability) values ('Clint','Freiheit','15');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (1, 'Painting1-1', 1995, '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (1, 'Painting1-2', 1997, '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (1, 'Painting1-3', 1985, '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (2, 'Painting1-1', 1995, '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (2, 'Painting1-2', 1997, '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (2, 'Painting1-3', 1985, '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (3, 'Painting1-1', 1995, '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (3, 'Painting1-2', 1997, '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (3, 'Painting1-3', 1985, '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (4, 'Painting1-1', 1995, '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (4, 'Painting1-2', 1997, '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
 
-insert into auction_paintings (painterID, title, dateOfWork, dateOfAuction, salePrice, height, widht, medium, subject)
+insert into auction_paintings (artistID, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
 values (4, 'Painting1-3', 1985, '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
+
+insert into inventory_paintings (artistID, title, dateOfWork, clasification, height, width, medium, subject, dateOfPurchase, 
+			nameOfSeller, addressOfSeller, maximunPurchasePrice, actualPurchasePrice, targetSellingPrice, sold, dateOfSale, 
+			nameOfBuyer, addressOfBuyer, actualSellingPrice) values (1, 'TestPainting1', 2001, 'Masterpiece', 12.0, 34.0, 
+			'Oil', 'computers', '20140122', 'Steve Shum', 'GSC', 10000, 50000, 200000, 1, '20140128', 'Jessica Spalding',
+			'Costelo', 400000.54);
+			
+			
+insert into inventory_paintings (artistID, title, dateOfWork, clasification, height, width, medium, subject, dateOfPurchase, 
+			nameOfSeller, addressOfSeller, maximunPurchasePrice, actualPurchasePrice, targetSellingPrice, sold, dateOfSale, 
+			nameOfBuyer, addressOfBuyer, actualSellingPrice) values (1, 'TestPainting2', 2001, 'Masterpiece', 12.0, 34.0, 
+			'Oil', 'computers', '20140122', 'Steve Shum', 'GSC', 10000, 50000, 200000, 1, '20110128', 'Jessica Spalding',
+			'Costelo', 400000.73);
+
+insert into inventory_paintings (artistID, title, dateOfWork, clasification, height, width, medium, subject, dateOfPurchase, 
+			nameOfSeller, addressOfSeller, maximunPurchasePrice, actualPurchasePrice, targetSellingPrice, sold, dateOfSale, 
+			nameOfBuyer, addressOfBuyer, actualSellingPrice) values (1, 'TestPainting3', 1981, 'Masterpiece', 110.0, 34.0, 
+			'Oil', 'computers', '20140122', 'Steve Shum', 'GSC', 10000, 50000, 200000, 1, '20110128', 'Jessica Spalding',
+			'Costelo', 400000.99); */
--- SQLFiles/SQLConnector.java
+++ SQLFiles/SQLConnector.java
@@ -1,36 +1,93 @@
-public class Artist{
+//Import required packages
+import java.sql.*;
+import java.util.*;
+
+public class SQLConnector{
 	
-	private String firstName;
-	private String lastName;
-	private int fashionabilityCoeff;
+	private static final String DATABASE_URL = "jdbc:mysql://localhost/LOO_database"; //Database URL
+	private Connection connection; //manages connection
+	private Statement statement;   //query statement
+	private ResultSet resultSet;   //manages results
+	private String sqlStatment;
 	
-	public Artist(){
-		firstName="";
-		lastName="";
-		fashionabilityCoeff = 0;
-	}
-	public Artist(String fName, String lName, int fCoeff){
-		firstName=fName;
-		lastName=lName;
-		fashionabilityCoeff = fCoeff;
-	}	
 	
-	public String getFirstName(){
-		return firstName;
+	public SQLConnector(String sqlStamt){
+		sqlStatment = sqlStamt;
 	}
-	public String getLastName(){
-		return lastName;
+	
+	public Vector executeSQL_Query(){
+		Vector querryReadyResult = new Vector(0);
+		try{
+			////establish connection to database
+			connection = DriverManager.getConnection(DATABASE_URL,"deitel","deitel");
+			//create Statement for query database
+			statement = connection.createStatement();
+			//query database
+			resultSet= statement.executeQuery(sqlStatment);			
+			querryReadyResult = processQuerryResult(resultSet);			
+		}
+		catch(SQLException sqlException)
+		{
+			sqlException.printStackTrace();
+		}
+		finally // ensure resultSet, statement and connection are closed
+		{
+			try
+			{
+				resultSet.close();
+				statement.close();
+				connection.close();
+			} 
+			catch ( Exception exception )
+			{
+			exception.printStackTrace();
+			} 
+		}
+		return querryReadyResult;
 	}
-	public int getFashionabilityCoeff(){
-		return f1ashionabilityCoeff;
+	
+	public int executeSQL_Update(){
+		int resultFlag = -1;
+		try{
+			////establish connection to database
+			connection = DriverManager.getConnection(DATABASE_URL,"deitel","deitel");
+			//create Statement for query database
+			statement = connection.createStatement();
+			//query database
+			resultFlag = statement.executeUpdate(sqlStatment);						
+		}
+		catch(SQLException sqlException)
+		{
+			sqlException.printStackTrace();
+		}
+		finally // ensure resultSet, statement and connection are closed
+		{
+			try
+			{
+				statement.close();
+				connection.close();
+			} 
+			catch ( Exception exception )
+			{
+			exception.printStackTrace();
+			} 
+		}	
+		return resultFlag;
 	}
-	public void setFirstName(String fName){
-		firstName=fName;
-	}
-	public void setLastName(String lName){
-		lastName=lName;
-	}
-	public void setFashionabilityCoeff(int fCoeff){
-		fashionabilityCoeff=fCoeff;
-	}
+	
+	public Vector processQuerryResult(ResultSet querryResultSet){
+		Vector querryReadyResult = new Vector(0);
+		try{
+			ResultSetMetaData metaData = resultSet.getMetaData();
+			int numberOfColums = metaData.getColumnCount();
+			while(querryResultSet.next()){
+				for(int i=1; i <= numberOfColums; i++)
+					querryReadyResult.add(querryResultSet.getObject(i));
+			}
+		}	
+		catch ( Exception exception ){
+			exception.printStackTrace();
+		} 
+		return querryReadyResult;
+	}		
 }
