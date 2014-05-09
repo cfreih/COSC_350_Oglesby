ab3d4baa2ed88adcfad83a1e4feea874ea736ab4
cva9222 <carcecascante10@ole.augie.edu>
2014-05-09 14:23:46 -0500

    GUI MAIN Inventory Panel

--- src/ManageInventoryMainMenuPanel.java
+++ src/ManageInventoryMainMenuPanel.java
@@ -1,69 +1,103 @@
-import java.awt.Font;
-
-import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
-import javax.swing.UIManager;
+import javax.swing.border.MatteBorder;
+
+import java.awt.Color;
+
+import javax.swing.border.BevelBorder;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import javax.swing.JButton;
+
+import javax.swing.SwingConstants;
+
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class ManageInventoryMainMenuPanel extends JPanel{
 	
-	private SpringLayout springLayout;
+	private GridBagLayout gridBagLayout;
 	private JButton SeeAllPaintingsButton;
 	private JButton ModifyPaintingButton;
 	private JButton AddNewPaintingButton;
 	private JButton ReturnToMainButton;
-	
-	public ManageInventoryMainMenuPanel()
-	{
-		springLayout = new SpringLayout();
-		
-		SeeAllPaintingsButton = new JButton("See All Paintings in Auction Record");
-		ModifyPaintingButton = new JButton("Modify/Update Existing Painting in Auction Records");
-		AddNewPaintingButton = new JButton("Add New Painting to Auction Records");
+
+	public ManageInventoryMainMenuPanel() {
+		gridBagLayout = new GridBagLayout();
+		SeeAllPaintingsButton = new JButton(
+				"See All Paintings in Inventory Record");
+		ModifyPaintingButton = new JButton(
+				"Modify/Update Existing Painting in Inventory Records");
+		AddNewPaintingButton = new JButton(
+				"Add New Painting to Inventory Records");
 		ReturnToMainButton = new JButton("Return to Main Menu");
-		
+
 		setUpPanel();
 	}
-	
-	public void setUpPanel()
-	{
-		setBorder(new CompoundBorder(new EtchedBorder(EtchedBorder.RAISED, null, null), new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Manage Auction Records", TitledBorder.CENTER, TitledBorder.ABOVE_TOP, null, null)));
-		
-		setLayout(springLayout);
-		
-		add(SeeAllPaintingsButton);		
+
+	public void setUpPanel() {
+		setBorder(new CompoundBorder(new EtchedBorder(EtchedBorder.RAISED,
+				null, null), new TitledBorder(
+				UIManager.getBorder("TitledBorder.border"),
+				"Manage Inventory Records", TitledBorder.CENTER,
+				TitledBorder.ABOVE_TOP, null, null)));
+
+		gridBagLayout.columnWidths = new int[] { 37, 250, 0 };
+		gridBagLayout.rowHeights = new int[] { 95, 50, 74, 51, 74, 50, 74, 50,
+				0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, Double.MIN_VALUE };
+		setLayout(gridBagLayout);
+
+		GridBagConstraints gbc_SeeAllPaintingsButton = new GridBagConstraints();
+		gbc_SeeAllPaintingsButton.fill = GridBagConstraints.BOTH;
+		gbc_SeeAllPaintingsButton.insets = new Insets(0, 0, 5, 0);
+		gbc_SeeAllPaintingsButton.gridx = 1;
+		gbc_SeeAllPaintingsButton.gridy = 1;
+		add(SeeAllPaintingsButton, gbc_SeeAllPaintingsButton);
 		SeeAllPaintingsButton.setFont(new Font("Century", Font.PLAIN, 12));
 		SeeAllPaintingsButton.setMnemonic('S');
-		springLayout.putConstraint(SpringLayout.NORTH, SeeAllPaintingsButton, 60, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.WEST, SeeAllPaintingsButton, 30, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, SeeAllPaintingsButton, 110, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, SeeAllPaintingsButton, 280, SpringLayout.WEST, this);
-		
-		add(ModifyPaintingButton);
+
+		GridBagConstraints gbc_ModifyPaintingButton = new GridBagConstraints();
+		gbc_ModifyPaintingButton.fill = GridBagConstraints.BOTH;
+		gbc_ModifyPaintingButton.insets = new Insets(0, 0, 5, 0);
+		gbc_ModifyPaintingButton.gridx = 1;
+		gbc_ModifyPaintingButton.gridy = 3;
+		add(ModifyPaintingButton, gbc_ModifyPaintingButton);
 		ModifyPaintingButton.setFont(new Font("Century", Font.PLAIN, 12));
 		ModifyPaintingButton.setMnemonic('M');
-		springLayout.putConstraint(SpringLayout.NORTH, ModifyPaintingButton, 70, SpringLayout.SOUTH, SeeAllPaintingsButton);
-		springLayout.putConstraint(SpringLayout.WEST, ModifyPaintingButton, 0, SpringLayout.WEST, SeeAllPaintingsButton);
-		springLayout.putConstraint(SpringLayout.SOUTH, ModifyPaintingButton, -342, SpringLayout.SOUTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, ModifyPaintingButton, 0, SpringLayout.EAST, SeeAllPaintingsButton);
-		
-		add(AddNewPaintingButton);
+
+		GridBagConstraints gbc_AddNewPaintingButton = new GridBagConstraints();
+		gbc_AddNewPaintingButton.fill = GridBagConstraints.BOTH;
+		gbc_AddNewPaintingButton.insets = new Insets(0, 0, 5, 0);
+		gbc_AddNewPaintingButton.gridx = 1;
+		gbc_AddNewPaintingButton.gridy = 5;
+		add(AddNewPaintingButton, gbc_AddNewPaintingButton);
 		AddNewPaintingButton.setFont(new Font("Century", Font.PLAIN, 12));
 		AddNewPaintingButton.setMnemonic('A');
-		springLayout.putConstraint(SpringLayout.NORTH, AddNewPaintingButton, 70, SpringLayout.SOUTH, ModifyPaintingButton);
-		springLayout.putConstraint(SpringLayout.WEST, AddNewPaintingButton, -250, SpringLayout.EAST, SeeAllPaintingsButton);
-		springLayout.putConstraint(SpringLayout.SOUTH, AddNewPaintingButton, 120, SpringLayout.SOUTH, ModifyPaintingButton);
-		springLayout.putConstraint(SpringLayout.EAST, AddNewPaintingButton, 0, SpringLayout.EAST, SeeAllPaintingsButton);
-		
-		add(ReturnToMainButton);
+
+		GridBagConstraints gbc_ReturnToMainButton = new GridBagConstraints();
+		gbc_ReturnToMainButton.fill = GridBagConstraints.BOTH;
+		gbc_ReturnToMainButton.gridx = 1;
+		gbc_ReturnToMainButton.gridy = 7;
+		add(ReturnToMainButton, gbc_ReturnToMainButton);
 		ReturnToMainButton.setFont(new Font("Century", Font.PLAIN, 12));
 		ReturnToMainButton.setMnemonic('R');
-		springLayout.putConstraint(SpringLayout.NORTH, ReturnToMainButton, 70, SpringLayout.SOUTH, AddNewPaintingButton);
-		springLayout.putConstraint(SpringLayout.WEST, ReturnToMainButton, 0, SpringLayout.WEST, SeeAllPaintingsButton);
-		springLayout.putConstraint(SpringLayout.SOUTH, ReturnToMainButton, 120, SpringLayout.SOUTH, AddNewPaintingButton);
-		springLayout.putConstraint(SpringLayout.EAST, ReturnToMainButton, 250, SpringLayout.WEST, SeeAllPaintingsButton);
-	}		
+	}
+	public JButton getSeeAllPaintingsButton() {
+		return SeeAllPaintingsButton;
+	}
+	public JButton getModifyPaintingButton() {
+		return ModifyPaintingButton;
+	}
+	public JButton getAddNewPaintingButton() {
+		return AddNewPaintingButton;
+	}
+	public JButton getReturnToMainButton() {
+		return ReturnToMainButton;
+	}
 }
