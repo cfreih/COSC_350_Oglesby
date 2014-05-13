0e69ceb25cd6b71b7147755ca03df4fbcbe24126
cva9222 <carcecascante10@ole.augie.edu>
2014-05-12 21:56:29 -0500

    Auto load Paintings into DATABASE
    Painitngs should be modified for real testing

--- src/LOO_database.sql
+++ src/LOO_database.sql
@@ -53,16 +53,14 @@ CREATE TABLE inventory_paintings (
 	dateOfSale INT,
 	nameOfBuyer varchar(30),
 	addressOfBuyer varchar(40),
-	actualSellingPrice DECIMAL(18,2),
-	flagBoughtReport BOOLEAN DEFAULT FALSE,
-	flagSoldReport BOOLEAN DEFAULT FALSE,	
+	actualSellingPrice DECIMAL(18,2),	
 	INDEX(dateOfPurchase),
 	INDEX(dateOfSale),
 	PRIMARY KEY(inventoryPaintingID)
 )
 ;
  
-/* insert into artists (firstName, lastName, fashionability) values ('Sam','Bock','5522');
+insert into artists (firstName, lastName, fashionability) values ('Sam','Bock','5522');
 insert into artists (firstName, lastName, fashionability) values ('Claudio','Arce','4000');
 insert into artists (firstName, lastName, fashionability) values ('Tim','Burwitz','6000');
 insert into artists (firstName, lastName, fashionability) values ('Clint','Freiheit','8900');
@@ -74,58 +72,69 @@ insert into artists (firstName, lastName, fashionability) values ('Steve','Shum'
 
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Sam','Bock', 'Painting1-1', 1995, '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
+values ('Sam','Bock', 'Painting1-1', '1995', '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Sam','Bock', 'Painting1-2', 1997, '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
+values ('Sam','Bock', 'Painting1-2', '1997', '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Sam','Bock', 'Painting1-3', 1985, '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
+values ('Sam','Bock', 'Painting1-3', '1985', '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Claudio','Arce', 'Painting1-1', 1995, '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
+values ('Claudio','Arce', 'Painting1-1', '1995?', '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Claudio','Arce', 'Painting1-2', 1997, '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
+values ('Claudio','Arce', 'Painting1-2', '1997?', '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Claudio','Arce', 'Painting1-3', 1985, '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
+values ('Claudio','Arce', 'Painting1-3', '1985', '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Tim','Burwitz', 'Painting1-1', 1995, '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
+values ('Tim','Burwitz', 'Painting1-1', '1995', '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Tim','Burwitz', 'Painting1-2', 1997, '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
+values ('Tim','Burwitz', 'Painting1-2', '1997', '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Tim','Burwitz', 'Painting1-3', 1985, '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
+values ('Tim','Burwitz', 'Painting1-3', '1985', '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Clint','Freiheit', 'Painting1-1', 1995, '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
+values ('Clint','Freiheit', 'Painting1-1', '1995', '20100322', 200000.55, 12.10, 15.10, 'Oil', 'Lunar');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Clint','Freiheit', 'Painting1-2', 1997, '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
+values ('Clint','Freiheit', 'Painting1-2', '1997', '20100422', 3100000.55, 24.10, 15.10, 'Canvas', 'Room');
 
 insert into auction_paintings (firstName, lastName, title, dateOfWork, dateOfAuction, salePrice, height, width, medium, subject)
-values ('Clint','Freiheit', 'Painting1-3', 1985, '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
+values ('Clint','Freiheit', 'Painting1-3', '1985', '20020322', 5000000.55, 23.10, 75.10, 'Water', 'House');
 
-insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, 
-			nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, 
-			nameOfBuyer, addressOfBuyer, actualSellingPrice) values ('Sam','Bock', 'TestPainting1', 2001, 'Masterpiece', 12.0, 34.0, 
-			'Oil', 'computers', '20140122', 'Steve Shum', 'GSC', 10000, 50000, '20140128', 'Jessica Spalding',
-			'Costelo', 400000.54);
-			
-			
-insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, 
-			nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, 
-			nameOfBuyer, addressOfBuyer, actualSellingPrice) values ('Sam','Bock', 'TestPainting2', 2001, 'Masterpiece', 12.0, 34.0, 
-			'Oil', 'computers', '20140122', 'Steve Shum', 'GSC', 10000, 50000, NULL, '',
-			'', 0);
- */
 
-/* insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, 
-			nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, 
-			nameOfBuyer, addressOfBuyer, actualSellingPrice) values ('Sam','Bock', 'TestPainting3', 1981, 'Masterpiece', 110.0, 34.0, 
-			'Oil', 'computers', '20140122', 'Steve Shum', 'GSC', 10000, 50000, '20110128', 'Jessica Spalding',
-			'Costelo', 400000.99);
- */
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Sam', 'Bock', 'TestPainting-1', '1825', 'Masterpiece', 12.0, 34.0, 'Oil', 'computers', 20140122, 'Steve Shum', 'GSC', 10000, 50000, 20140128, 'Jessica Spalding', 'Costelo', 400000.54);
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Sam','Bock', 'TestPainting-2', '2004',     'Masterwork',  12.0,     34.0,   'Oil',   'computers', 20140122,         'Steve Shum', 'GSC',           10000,                50000,               20140128,   'Jessica Spalding', 'Costelo',      400000.54);
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Claudio',     'Arce',   'TestPainting-3', '1600?',     'Other',  12.0,     34.0,   'Oil',   'computers', 20140122,         'Steve Shum', 'GSC',           10000,                50000,               null,   null, null,      null);
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Clint',     'Freiheit',   'TestPainting-4', '1756',     'Other',  12.0,     34.0,   'Oil',   'computers', 20140122,         'Steve Shum', 'GSC',           10000,                50000,               20140128,   'Jessica Spalding', 'Costelo',      400000.54);
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Clint',     'Freiheit',   'TestPainting-5', '1754',     'Masterwork',  12.0,     34.0,   'Oil',   'computers', 20140122,         'Steve Shum', 'GSC',           10000,                50000,               20140128,   'Jessica Spalding', 'Costelo',      400000.54);
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Steve',     'Shum',   'TestPainting-6', '1564',     'Masterwork',  12.0,     34.0,   'Oil',   'computers', 20140122,         'Steve Shum', 'GSC',           10000,                50000,               null,   null, null,      null);
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Sam',     'Bock',   'TestPainting-7', '1801',     'Masterpiece',  12.0,     34.0,   'Oil',   'computers', 20140122,         'Steve Shum', 'GSC',           10000,                50000,               20140128,   'Jessica Spalding', 'Costelo',      400000.54);
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Tim',     'Burwitz',   'TestPainting-8', '2001',     'Masterwork',  12.0,     34.0,   'Oil',   'computers', 20140122,         'Steve Shum', 'GSC',           10000,                50000,               null,   null, null,      null);
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Tim',     'Burwitz',   'TestPainting-9', '1525',     'Masterpiece',  12.0,     34.0,   'Oil',   'computers', 20140122,         'Steve Shum', 'GSC',           10000,                50000,               20140128,   'Jessica Spalding', 'Costelo',      400000.54);            
+
+insert into inventory_paintings (firstName, lastName, title, dateOfWork, classification, height, width, medium, subject, dateOfPurchase, nameOfSeller, addressOfSeller, maximumPurchasePrice, actualPurchasePrice, dateOfSale, nameOfBuyer, addressOfBuyer, actualSellingPrice)
+values ('Steve',     'Shum',   'TestPainting-10', '1725',     'Masterpiece',  12.0,     34.0,   'Oil',   'computers', 20140122,         'Steve Shum', 'GSC',           10000,                50000,               null,   null, null,      null);            
