35762b9c9e6579139094f77ea4e1b0bc4c2a95e6
tburwitz <tgburwitz10@ole.augie.edu>
2014-05-13 22:25:32 -0500

    Added Finished Tables to two of the artists panels

--- src/SearchResultsArtistPanel.java
+++ src/SearchResultsArtistPanel.java
@@ -1,106 +1,119 @@
 import javax.swing.JPanel;
-import java.awt.GridBagLayout;
-import javax.swing.JList;
-import java.awt.GridBagConstraints;
-import javax.swing.JButton;
-import java.awt.Insets;
+import javax.swing.JFrame;
 import javax.swing.JScrollPane;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import javax.swing.border.TitledBorder;
 
+import java.awt.Font;
+
+import javax.swing.ListSelectionModel;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.JButton;
+import javax.swing.JList;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class SearchResultsArtistPanel extends JPanel {
-	
-	private GridBagLayout gridBagLayout;
+	private JButton btnBack;
+	private JTable artistTable;
+	private DefaultTableModel tableModel;
 	private JButton btnSelect;
-	private JButton btnCancel;
-	private JScrollPane scrollPaneList;
-	private JList<Artist> listSearchResults;
-	private JLabel lblSearchResults;
-	private Artist[] searchResults;
-	
 	public SearchResultsArtistPanel() {
-		
-		gridBagLayout = new GridBagLayout();
-		scrollPaneList = new JScrollPane();
-		listSearchResults = new JList<Artist>();
-		lblSearchResults = new JLabel("Search Results");
-		btnSelect = new JButton("Select");
-		btnCancel = new JButton("Cancel");
-		
-		setUpPanel();
-	}
-	
-	public void setUpPanel()
-	{
-		setBorder(new TitledBorder(null, "Select Artist", TitledBorder.CENTER, TitledBorder.TOP, null, null));
-		
-		gridBagLayout.columnWidths = new int[]{0, 325, 0, 0, 325, 0};
-		gridBagLayout.rowHeights = new int[]{0, 400, 0, 34, 0};
+		setBorder(new CompoundBorder(new EtchedBorder(EtchedBorder.LOWERED,
+				null, null), new TitledBorder(null, "See All Artists",
+				TitledBorder.CENTER, TitledBorder.TOP, null, null)));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 150, 150, 150, 150, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 4;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 1;
+		gbc_scrollPane.gridy = 1;
+		add(scrollPane, gbc_scrollPane);
 		
-		GridBagConstraints gbc_scrollPaneList = new GridBagConstraints();
-		gbc_scrollPaneList.gridheight = 2;
-		gbc_scrollPaneList.gridwidth = 4;
-		gbc_scrollPaneList.insets = new Insets(0, 0, 5, 0);
-		gbc_scrollPaneList.fill = GridBagConstraints.BOTH;
-		gbc_scrollPaneList.gridx = 1;
-		gbc_scrollPaneList.gridy = 1;
-		add(scrollPaneList, gbc_scrollPaneList);
+		artistTable = new JTable();
+		tableModel =new DefaultTableModel(
+				new Object[][] {
+						{null, null, null},
+						{null, null, null},
+					},
+					new String[] {
+						"Last Name", "First Name", "Fashionability"
+					}
+				);
+		artistTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				"Last Name", "First Name", "Fashionability"
+			}
+		));
+		scrollPane.setViewportView(artistTable);
 		
-		
-		scrollPaneList.setViewportView(listSearchResults);		
-		lblSearchResults.setLabelFor(listSearchResults);
-		lblSearchResults.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSearchResults.setFont(new Font("Century", Font.PLAIN, 12));
-		scrollPaneList.setColumnHeaderView(lblSearchResults);
-		
-		
-		btnSelect.setFont(new Font("Century", Font.PLAIN, 12));
+		btnSelect = new JButton("Select");
 		GridBagConstraints gbc_btnSelect = new GridBagConstraints();
-		gbc_btnSelect.fill = GridBagConstraints.BOTH;
-		gbc_btnSelect.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSelect.fill = GridBagConstraints.HORIZONTAL;
+		gbc_btnSelect.insets = new Insets(0, 0, 5, 5);
 		gbc_btnSelect.gridx = 1;
-		gbc_btnSelect.gridy = 3;
+		gbc_btnSelect.gridy = 2;
 		add(btnSelect, gbc_btnSelect);
 		
-				
-		btnCancel.setFont(new Font("Century", Font.PLAIN, 12));
-		GridBagConstraints gbc_btnCancel = new GridBagConstraints();
-		gbc_btnCancel.fill = GridBagConstraints.BOTH;
-		gbc_btnCancel.gridx = 4;
-		gbc_btnCancel.gridy = 3;
-		add(btnCancel, gbc_btnCancel);
+		btnBack = new JButton("Back");
+		GridBagConstraints gbc_btnBack = new GridBagConstraints();
+		gbc_btnBack.fill = GridBagConstraints.BOTH;
+		gbc_btnBack.insets = new Insets(0, 0, 5, 5);
+		gbc_btnBack.gridx = 2;
+		gbc_btnBack.gridy = 2;
+		add(btnBack, gbc_btnBack);
 	}
-	
-	/**
-	 * Desc: Takes a searchPainting and sets the list to what that will
-	 * 		 retrieve from the auction, the search results.
-	 * Post: listSearchResults has the data of searchResults.
-	 */
-	public void updateSearchResultsList(Artist[] searchResults)
-	{
-		listSearchResults.setListData(searchResults);
-	}
-	
-	/**
-	 * Desc: Gets the selected painting and returns what it is.
-	 * @return the painting selected in the listSearchResults.
-	 */
-	public Artist getSelectedPainting()
-	{
-		return listSearchResults.getSelectedValue();
-	}
+	public void updateTableModel(Artist[] artists){
+		Object[][] dataVector= new Object[artists.length][20];
+		for(int i=0; i <= artists.length-1; i++)
+			dataVector[i]=artists[i].toTableRow();
+		String[] columnNames = new String[] {
+				"Artist First Name", "Arist Last Name", "Fashionability"};
+		tableModel.setDataVector(dataVector, columnNames);
+		artistTable.setModel(tableModel);
+		artistTable.getColumnModel().getColumn(0).setMinWidth(200);
+		artistTable.getColumnModel().getColumn(1).setMinWidth(200);
+		artistTable.getColumnModel().getColumn(2).setMinWidth(100);
+		artistTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		}
 
-	public JButton getBtnSelect() {
-		return btnSelect;
+	public JButton getBtnBack() {
+		return btnBack;
 	}
-	public JButton getBtnCancel() {
-		return btnCancel;
+	public static void main(String[] args) {
+		JFrame f = new JFrame("test window");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		f.setLocationRelativeTo(null);
+		f.setSize(800, 600);
+		f.setLocation(10, 10);
+		SearchResultsArtistPanel panel =new SearchResultsArtistPanel();
+		f.getContentPane().add(panel);
+		f.setVisible(true);
+		Artist[] artists = new Artist[75];
+		for(int i=0; i <= artists.length-1; ++i){
+			
+			artists[i] = new Artist( "thisnameis25characterslon","lastName", 5000);
+		}
+		panel.updateTableModel(artists);
 	}
 }
--- src/SeeAllArtistsPanel.java
+++ src/SeeAllArtistsPanel.java
@@ -1,22 +1,32 @@
 import javax.swing.JPanel;
+import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+
 import java.awt.Font;
+
 import javax.swing.ListSelectionModel;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.JButton;
 import javax.swing.JList;
+
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.Dimension;
+import java.awt.BorderLayout;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class SeeAllArtistsPanel extends JPanel {
 	private JButton btnBack;
+	private JTable artistTable;
+	private DefaultTableModel tableModel;
 	public SeeAllArtistsPanel() {
 		setBorder(new CompoundBorder(new EtchedBorder(EtchedBorder.LOWERED,
-				null, null), new TitledBorder(null, "Manage Auction Records",
+				null, null), new TitledBorder(null, "See All Artists",
 				TitledBorder.CENTER, TitledBorder.TOP, null, null)));
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0, 0};
@@ -33,11 +43,26 @@ public class SeeAllArtistsPanel extends JPanel {
 		gbc_scrollPane.gridy = 1;
 		add(scrollPane, gbc_scrollPane);
 		
-		JList listAllArtists = new JList();
-		listAllArtists.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		listAllArtists.setVisibleRowCount(40);
-		listAllArtists.setSize(new Dimension(1, 3));
-		scrollPane.setViewportView(listAllArtists);
+		artistTable = new JTable();
+		tableModel =new DefaultTableModel(
+				new Object[][] {
+						{null, null, null},
+						{null, null, null},
+					},
+					new String[] {
+						"Last Name", "First Name", "Fashionability"
+					}
+				);
+		artistTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+			},
+			new String[] {
+				"Last Name", "First Name", "Fashionability"
+			}
+		));
+		scrollPane.setViewportView(artistTable);
 		
 		btnBack = new JButton("Back");
 		GridBagConstraints gbc_btnBack = new GridBagConstraints();
@@ -45,8 +70,38 @@ public class SeeAllArtistsPanel extends JPanel {
 		gbc_btnBack.gridy = 3;
 		add(btnBack, gbc_btnBack);
 	}
+	public void updateTableModel(Artist[] artists){
+		Object[][] dataVector= new Object[artists.length][20];
+		for(int i=0; i <= artists.length-1; i++)
+			dataVector[i]=artists[i].toTableRow();
+		String[] columnNames = new String[] {
+				"Artist First Name", "Arist Last Name", "Fashionability"};
+		tableModel.setDataVector(dataVector, columnNames);
+		artistTable.setModel(tableModel);
+		artistTable.getColumnModel().getColumn(0).setMinWidth(200);
+		artistTable.getColumnModel().getColumn(1).setMinWidth(200);
+		artistTable.getColumnModel().getColumn(2).setMinWidth(100);
+		artistTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		}
 
 	public JButton getBtnBack() {
 		return btnBack;
+	}
+	public static void main(String[] args) {
+		JFrame f = new JFrame("test window");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		f.setLocationRelativeTo(null);
+		f.setSize(800, 600);
+		f.setLocation(10, 10);
+		SeeAllArtistsPanel panel =new SeeAllArtistsPanel();
+		f.getContentPane().add(panel);
+		f.setVisible(true);
+		Artist[] artists = new Artist[75];
+		for(int i=0; i <= artists.length-1; ++i){
+			
+			artists[i] = new Artist( "thisnameis25characterslon","lastName", 5000);
+		}
+		panel.updateTableModel(artists);
 	}
 }
