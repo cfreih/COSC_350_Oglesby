10ce9a6b6203398bfbd90f47907d16f306fd382d
cva9222 <carcecascante10@ole.augie.edu>
2014-05-13 21:40:36 -0500

    See all Works

--- src/SeeAllInventoryPanel.java
+++ src/SeeAllInventoryPanel.java
@@ -6,6 +6,8 @@ import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 import java.awt.Color;
+
+import javax.swing.JFrame;
 import javax.swing.SpringLayout;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -15,6 +17,7 @@ import javax.swing.JButton;
 
 public class SeeAllInventoryPanel extends JPanel {
 	private JTable paintingsTable;
+	private DefaultTableModel tableModel;
 	
 	public SeeAllInventoryPanel() {
 		
@@ -34,15 +37,16 @@ public class SeeAllInventoryPanel extends JPanel {
 		add(scrollPane);
 		
 		paintingsTable = new JTable();
-		paintingsTable.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
-			},
-			new String[] {
-				"Last Name", "First Name", "Title", "Date of Work", "Classification", "Height", "Width", "Medium", "Subject", "Date of Prucahse", "Name of Seller", "Address of Seller", "Maximun Purchase Price", "Actual Purchase Price", "Target Selling Price", "Date of Sale", "Name of Buyer", "Address of Buyer", "Actual Selling Price", "New column"
-			}
-		));
+		tableModel =new DefaultTableModel(
+				new Object[][] {
+						{null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
+						{null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
+					},
+					new String[] {
+						"Last Name", "First Name", "Title", "Date of Work", "Classification", "Height", "Width", "Medium", "Subject", "Date of Prucahse", "Name of Seller", "Address of Seller", "Maximun Purchase Price", "Actual Purchase Price", "Target Selling Price", "Date of Sale", "Name of Buyer", "Address of Buyer", "Actual Selling Price", "New column"
+					}
+				);
+		paintingsTable.setModel(tableModel);
 		paintingsTable.getColumnModel().getColumn(9).setPreferredWidth(96);
 		paintingsTable.getColumnModel().getColumn(10).setPreferredWidth(92);
 		paintingsTable.getColumnModel().getColumn(11).setPreferredWidth(95);
@@ -55,5 +59,44 @@ public class SeeAllInventoryPanel extends JPanel {
 		springLayout.putConstraint(SpringLayout.NORTH, btnBack, 5, SpringLayout.SOUTH, scrollPane);
 		springLayout.putConstraint(SpringLayout.WEST, btnBack, 10, SpringLayout.WEST, scrollPane);
 		add(btnBack);
+	}
+	public void updateTableModel(InventoryPainting[] invPainintgs){
+		
+		Object[][] dataVector= new Object[invPainintgs.length][20];
+		
+		for(int i=0; i <= invPainintgs.length-1; i++){
+		
+			dataVector[i]=invPainintgs[i].toTableRow();
+		
+		}		
+		//Object[][] dataVector = {invPainintgs[0].toTableRow(),invPainintgs[1].toTableRow(),invPainintgs[2].toTableRow(),invPainintgs[3].toTableRow(),invPainintgs[4].toTableRow()};
+		
+		String[] columnNames = new String[] {
+				"Artist First Name", "Arist Last Name", "Title", "Date of Work",
+				"Classification", "Height", "Width", "Medium", "Subject",
+				"Date of Purchase", "Name of Seller", "Address of Seller",
+				"Maximun Purchase Price", "Actual Purchase Price", "Target Selling Price",
+				"Date of Sale", "Name of Buyer", "Address of Buyer", "Actual Selling Price"};
+		tableModel.setDataVector(dataVector, columnNames);
+		paintingsTable.setModel(tableModel);
+	}
+	
+	public static void main(String[] args) {
+		JFrame f = new JFrame("test window");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		f.setLocationRelativeTo(null);
+		f.setSize(800, 600);
+		f.setLocation(10, 10);
+		SeeAllInventoryPanel panel =new SeeAllInventoryPanel();
+		f.getContentPane().add(panel);
+		f.setVisible(true);
+		InventoryPainting[] invP = new InventoryPainting[25];
+		for(int i=0; i <= invP.length-1; ++i){
+			
+			invP[i] = new InventoryPainting( "firstName","lastName","title","date",22.8,36.8,"med","subj","sName","sAddress",new SimpleDate(),15222.0, 175545.2,new SimpleDate(),"bName", "bAddress", 589999.0,
+					"classif");			
+		}
+		panel.updateTableModel(invP);
 	}
 }
